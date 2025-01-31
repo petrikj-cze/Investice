1ca6e01789dc7c7e7594f41451ba58148f3b399b
Jaroslav Petřík <jarek.petrik@proton.me>
2025-01-31 19:11:23 +0100

    První commit

---
+++ js/investice.js
@@ -0,0 +1,11 @@
+//Inicializace komponent
+document.getElementById("investment-date").valueAsDate = new Date();
+document.getElementById("investment-amount").value = "0 Kč";
+
+//načítaní komponent do proměnných
+
+
+//funkce
+function clearMoneyInput (str){
+	const regex = /Kč/;
+}
